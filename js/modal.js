9a7d9122b52ec64456a87172228e80733105e433
georgijbudik <georgijbudik@gmail.com>
2023-06-19 19:07:42 +0200

    add feature closemodal

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,40 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    form: document.querySelector(".modal-js-form"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", onOpenModal);
+  refs.closeModalBtn.addEventListener("click", onCloseModal);
+  refs.modal.addEventListener("click", onBackdropClick);
+  refs.form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  // function toggleModal() {
+  //   refs.modal.classList.toggle("is-hidden");
+  // }
+
+  function onOpenModal() {
+    window.addEventListener("keydown", onEscPress);
+    refs.modal.classList.remove("is-hidden");
+  }
+
+  function onCloseModal() {
+    window.removeEventListener("keydown", onEscPress);
+    refs.modal.classList.add("is-hidden");
+  }
+
+  function onEscPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    if (event.code === ESC_KEY_CODE) {
+      onCloseModal();
+    }
+  }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
   }
 })();
